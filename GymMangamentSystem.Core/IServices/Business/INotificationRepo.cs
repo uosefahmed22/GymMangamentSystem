400de997269e45ada16672471a230d597f897005
UOSEF <UOSEF@DESKTOP-SVVTNST>
2024-08-18 03:59:13 +0300

    *

---
+++ GymMangamentSystem.Core/IServices/Business/INotificationRepo.cs
@@ -0,0 +1,17 @@
+﻿using GymMangamentSystem.Core.Dtos.Business;
+using GymMangamentSystem.Core.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangamentSystem.Core.IServices.Business
+{
+    public interface INotificationRepo
+    {
+        Task<IEnumerable<NotificationDto>> GetNotifications();
+        Task<ApiResponse> AddNotification(NotificationDto notificationDto);
+        Task<ApiResponse> DeleteNotification(int notificationId);
+    }
+}
---
+++ GymMangamentSystem/Controllers/NotificationController.cs
@@ -0,0 +1,64 @@
+﻿using GymMangamentSystem.Core.Dtos.Business;
+using GymMangamentSystem.Core.IServices.Business;
+using GymMangamentSystem.Core.Models.Business;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMangamentSystem.Apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationRepo _notification;
+
+        public NotificationController(INotificationRepo notification)
+        {
+            _notification = notification;
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetNotifications()
+        {
+            try
+            {
+                var notifications = await _notification.GetNotifications();
+                return Ok(notifications);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [Authorize(Roles = "Admin, Receptionist")]
+        [HttpPost]
+        public async Task<IActionResult> AddNotification([FromBody] NotificationDto notification)
+        {
+            try
+            {
+                var response = await _notification.AddNotification(notification);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [Authorize(Roles = "Admin, Receptionist")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteNotification(int id)
+        {
+            try
+            {
+                var response = await _notification.DeleteNotification(id);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
